e6ade55f2f245bbe725a9a0695bb86f94f9814e0
masudcns <masudur07cns@gmail.com>
2025-05-08 13:14:01 +0530

    move slider and announcement route into white lable

---
+++ backend/models/announcement.model.js
@@ -0,0 +1,30 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../db.js';
+import gameSchema from './game.model.js';
+
+const announcementSchema = sequelize.define(
+  'announcement',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    announceId: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    announcement: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'announcements',
+    tableName: 'announcements',
+    timestamps: true,
+  },
+);
+
+export default announcementSchema;
